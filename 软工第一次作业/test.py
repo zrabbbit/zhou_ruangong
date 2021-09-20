db911adc6127eb6afce77dfb193eb8c2c7172df0
zrabbbit <1374803499@qq.com>
2021-09-20 15:45:08 +0800

    style: change path

--- 软工第一次作业/test.py
+++ 软工第一次作业/test.py
@@ -1,35 +1,35 @@
 import unittest
 
-from 统计关键字个数4 import total_count
-from 统计关键字个数4 import switch_count
-from 统计关键字个数4 import if_elseif_else_count
+from 统计关键字个数5 import total_count
+from 统计关键字个数5 import switch_count
+from 统计关键字个数5 import if_elseif_else_count
 
 
 class TotalCountTest(unittest.TestCase):
     # 用于测试 total_count 函数
     def test_file_str(self):
-        fo = open("D:\\Code\\样例.c", "r")
+        fo = open("D:\\Code\\example.cpp", "r")
         total_num = total_count(fo)
         fo.close()
-        self.assertEqual(total_num, 35)  # add assertion here
+        self.assertEqual(total_num, 20)  # add assertion here
 
 
 class SwitchCountTest(unittest.TestCase):
     # 用于测试 switch_count 函数
     def test_file_str(self):
-        fo = open("D:\\Code\\样例.c", "r")
+        fo = open("D:\\Code\\example.cpp", "r")
         switch_num = switch_count(fo)
         fo.close()
-        self.assertEqual(switch_num, 1)
+        self.assertEqual(switch_num + 1, 1)
 
 
 class IfElseifCountTest(unittest.TestCase):
     # 用于测试 if_elseif_else 函数
     def test_file_str(self):
-        fo = open("D:\\Code\\样例.c", "r")
+        fo = open("D:\\Code\\example.cpp", "r")
         if_else_num = if_elseif_else_count(fo)
         fo.close()
-        self.assertEqual(if_else_num, 2)
+        self.assertEqual(if_else_num, 1)
 
 
 if __name__ == '__main__':
